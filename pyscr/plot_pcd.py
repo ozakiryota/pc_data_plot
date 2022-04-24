d43cf8349bba61ff3d10056ba300c2b5644fe164
ozakiryota <kb22056@gmail.com>
2022-04-24 20:15:02 +0900

    Release

--- pyscr/plot_pcd.py
+++ pyscr/plot_pcd.py
@@ -15,11 +15,13 @@ class PlotPcd:
     def setArgument(self):
         self.arg_parser.add_argument('--pcd_path', required=True)
         self.arg_parser.add_argument('--save_dir', default='../save')
-        # self.arg_parser.add_argument('--num_w', type=int, required=True)
-        # self.arg_parser.add_argument('--num_h', type=int)
-        # self.arg_parser.add_argument('--flag_label', action='store_true')
-        # self.arg_parser.add_argument('--flag_tight_layout', action='store_true')
         self.arg_parser.add_argument('--flag_merge', action='store_true')
+        self.arg_parser.add_argument('--x_min', type=float)
+        self.arg_parser.add_argument('--x_max', type=float)
+        self.arg_parser.add_argument('--y_min', type=float)
+        self.arg_parser.add_argument('--y_max', type=float)
+        self.arg_parser.add_argument('--z_min', type=float)
+        self.arg_parser.add_argument('--z_max', type=float)
         return self.arg_parser.parse_args()
 
     def main(self):
@@ -40,13 +42,49 @@ class PlotPcd:
                 points_z_list.append(points[:, 2])
             else:
                 pcd_name = os.path.splitext(os.path.basename(pcd_path))[0]
-                self.plotAndSave(os.path.join(pcd_name, 'yx.png'), 'y [m]', 'x [m]', [points[:, 1]], [points[:, 0]], invert_x_axis=True)
-                self.plotAndSave(os.path.join(pcd_name, 'yz.png'), 'y [m]', 'z [m]', [points[:, 1]], [points[:, 2]], invert_x_axis=True)
-                self.plotAndSave(os.path.join(pcd_name, 'xz.png'), 'x [m]', 'z [m]', [points[:, 0]], [points[:, 2]], invert_x_axis=False)
+                self.plotAndSave(
+                    os.path.join(pcd_name, 'yx.png'),
+                    'y [m]', 'x [m]',
+                    [points[:, 1]], [points[:, 0]],
+                    invert_x_axis=True,
+                    xlim=[self.args.y_min, self.args.y_max], ylim=[self.args.x_min, self.args.x_max]
+                )
+                self.plotAndSave(
+                    os.path.join(pcd_name, 'yz.png'),
+                    'y [m]', 'z [m]',
+                    [points[:, 1]], [points[:, 2]],
+                    invert_x_axis=True,
+                    xlim=[self.args.y_min, self.args.y_max], ylim=[self.args.z_min, self.args.z_max]
+                )
+                self.plotAndSave(
+                    os.path.join(pcd_name, 'xz.png'),
+                    'x [m]', 'z [m]',
+                    [points[:, 0]], [points[:, 2]],
+                    invert_x_axis=False,
+                    xlim=[self.args.x_min, self.args.x_max], ylim=[self.args.z_min, self.args.z_max]
+                )
         if self.args.flag_merge:
-            self.plotAndSave('yx.png', 'y [m]', 'x [m]', points_y_list, points_x_list, invert_x_axis=True)
-            self.plotAndSave('yz.png', 'y [m]', 'z [m]', points_y_list, points_z_list, invert_x_axis=True)
-            self.plotAndSave('xz.png', 'x [m]', 'z [m]', points_x_list, points_z_list, invert_x_axis=False)
+            self.plotAndSave(
+                'yx.png',
+                'y [m]', 'x [m]',
+                points_y_list, points_x_list,
+                invert_x_axis=True,
+                xlim=[self.args.y_min, self.args.y_max], ylim=[self.args.x_min, self.args.x_max]
+            )
+            self.plotAndSave(
+                'yz.png',
+                'y [m]', 'z [m]',
+                points_y_list, points_z_list,
+                invert_x_axis=True,
+                xlim=[self.args.y_min, self.args.y_max], ylim=[self.args.z_min, self.args.z_max]
+            )
+            self.plotAndSave(
+                'xz.png',
+                'x [m]', 'z [m]',
+                points_x_list, points_z_list,
+                invert_x_axis=False,
+                xlim=[self.args.x_min, self.args.x_max], ylim=[self.args.z_min, self.args.z_max]
+            )
         # plt.show()
 
     def getPcdList(self):
@@ -58,24 +96,30 @@ class PlotPcd:
                 pcd_path_list.append(file_path)
         return pcd_path_list
 
-    def plotAndSave(self, save_name, x_label, y_label, x_data_list, y_data_list, invert_x_axis=False, invert_y_axis=False):
+    def plotAndSave(self, save_name, x_label, y_label, x_data_list, y_data_list, invert_x_axis=False, invert_y_axis=False, xlim=[None, None], ylim=[None, None]):
+        ## path
         save_path = os.path.join(self.args.save_dir, save_name)
         save_dir = os.path.dirname(save_path)
-
+        ## figure
         plt.figure()
         sub = plt.subplot()
-
+        ## label
         plt.xlabel(x_label)
         plt.ylabel(y_label)
+        ## data
         for x_data, y_data in zip(x_data_list, y_data_list):
             plt.scatter(x_data, y_data, s=1)
-
+        ## axis
+        if (xlim[0] is not None) and (xlim[1] is not None):
+            sub.set_xlim(xlim)
+        if (ylim[0] is not None) and (ylim[1] is not None):
+            sub.set_ylim(ylim)
         if invert_x_axis:
             sub.invert_xaxis()
         if invert_y_axis:
             sub.invert_yaxis()
         sub.set_aspect('equal')
-
+        ## save
         os.makedirs(save_dir, exist_ok=True)
         plt.savefig(save_path)
         print('Save:', save_path)
--- shell/plot_sample.sh
+++ shell/plot_sample.sh
@@ -6,4 +6,8 @@ pcd_dir_name=${pcd_dir##*/}
 python3 ../pyscr/plot_pcd.py \
     --pcd_path $pcd_dir/\* \
     --save_dir ../save/$pcd_dir_name \
-    --flag_merge
+    --flag_merge \
+    --x_min -50 \
+    --x_max 50 \
+    --y_min -100 \
+    --y_max 100
